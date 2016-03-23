d6de7be7b06036b77d3aa6a736b28b3dd2fae040
unknown <marki423@student.liu.se>
2016-03-23 18:02:13 +0100

    multiple enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,10 @@ using System;
     class Enemy : MonoBehaviour
     {
     public GameObject enemy;
+    public Renderer rend;
+    public Color altColor = Color.black;
 
-        public Enemy()
+    public Enemy()
         {
         enemy = new GameObject();
         enemy.name = "Enemy";
@@ -16,12 +18,13 @@ using System;
         GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.SetParent(enemy.transform);
 
         enemy.AddComponent<MeshRenderer>();
-        Material runtimeMaterial = new Material(Shader.Find("Standard"));
-        runtimeMaterial.color = new Color(1.0f, 0, 0);
-        enemy.GetComponent<Renderer>().material = runtimeMaterial;
-        //enemy.transform.position = new Vector3(1.5f, 0, 0);
+        
         enemy.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-        }
+
+        rend = enemy.GetComponent<Renderer>();
+        rend.material.color = altColor;
+        enemy.GetComponent<Renderer>().material.color = new Color(1, 0, 1, 0);
+    }
 
         public void setEnemyLocation(float x, float y)
         {
