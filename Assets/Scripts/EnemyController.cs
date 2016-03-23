c6fe8b56a9eb60ea2ddc1df1225bfb6bf4eca21c
Niklas Fransson <nikfr357@student.liu.se>
2016-03-23 18:03:44 +0100

    added greatest AI in the world

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+
+    private float speed = 3.0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Random.value;
+        float moveVertical = Random.value;
+
+        Debug.Log("X: " + Input.GetAxis("Horizontal") + " Y: " + Input.GetAxis("Vertical"));
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
